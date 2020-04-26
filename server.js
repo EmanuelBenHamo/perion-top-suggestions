1166e0c753f0a94339f33d8d416a5f43908902c9
Emanuel Ben Hamo <embenh@gmail.com>
2020-04-26 06:42:13 +0300

    updated cors to include client site url

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const searchRoutes = require('./api/search/search.routes');
 const searchService = require('./api/search/search.service');
 
 const app = express();
 const server = http.createServer(app);
+
+// tell the server where from it should serve the front end files
+
+const corsOptions = {
+    origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'https://search-suggestion-client.herokuapp.com'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 
 // routes
 app.use('/api/search', searchRoutes);
